ee8d3fea18a110f00e7bcdb2b667751e6406b4f1
lydiawijaya <lyliwijaya11@gmail.com>
2016-04-26 14:44:30 +0700

    menginput kompetensi baru
    kompetensi exception flow

--- dosen/input-kompetensi.php
+++ dosen/input-kompetensi.php
@@ -1,127 +1,203 @@
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
-<html xmlns="http://www.w3.org/1999/xhtml">
+<!DOCTYPE html>
+<html lang="en">
+
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
-<title>Untitled Document</title>
+
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Kompetensi</title>
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/Login.css" rel="stylesheet">
 </head>
 
 <body>
-<table width="735" height="60" border="1">
-  <tr>
-    <td width="58" height="29"><input type="submit" name="Submit" value="HOME" /></td>
-    <td width="60"><form id="form1" name="form1" method="post" action="">
-      <input type="submit" name="Submit2" value="PBM" />
-    </form>    </td>
-    <td width="64"><form id="form2" name="form2" method="post" action="">
-      <input type="submit" name="Submit3" value="FORM" />
-    </form>    </td>
-    <td width="525"><form id="form3" name="form3" method="post" action="">
-      <input type="submit" name="Submit4" value="LOGOUT" />
-    </form>    </td>
-  </tr>
-  <tr>
-    <td colspan="3">&nbsp;</td>
-    <td><form id="form4" name="form4" method="post" action="">
-      <input type="text" name="textfield" />
-          <input type="submit" name="Submit5" value="Search" />
-    </form></td>
-  </tr>
-</table>
-<table width="738" border="0">
-  <tr>
-    <td width="145"><label>1 identitas MA</label>
-    &nbsp;</td>
-    <td width="217"><label>2 GBPP</label></td>
-    <td width="113"><label>3 KONTRAK PERKULIAHAN</label>&nbsp;</td>
-    <td width="46"><label>4 SAP</label>&nbsp;</td>
-    <td width="195"><form id="form5" name="form5" method="post" action="">
-	 <?php
- class year{
-  protected $pilih;
-  public function  setPilih(array $pilih){
-        $this->pilih = array($pilih);
-    }
-	 public function getPilih(){
-        return $this->pilih;
-    }
- }
-?>
-			<?php
-$year = new year();
-$year->setPilih(array ("2010", "2011", "2012", "2013", "2014", "2015", "2016"));
-$tahunajaran = array("2010", "2011", "2012", "2013", "2014", "2015", "2016");
- 
- 
- ?>
-      Tahun ajaran
-      <select name="tahun">
-		<option value="">pilih tahun</option>
+    <nav class="navbar navbar-inverse navbar-fixed-top">
+        <div class="container">
+            
+            <div class="navbar-header">
+                <a class="navbar-brand">
+                    <img src="LogoEpbm1.png" alt="">
+                </a>
+            </div>
+            
+            <div id="navbar">
+            <ul class="nav navbar-nav">
+            
+            <li class="active"><a href="#">Home</a>
+            </li>
+<!--dropdown Menu----------------------------------------------------------->     
+            
+        <li class="dropdown">
+          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+            PBM<span class="caret"></span>
+          </a>
+            
+          <ul class="dropdown-menu">
+            <li><a href="#">Semester 1</a></li>
+            <li><a href="#">Semester 2</a></li>
+            <li><a href="#">Semester 3</a></li>
+            <li><a href="#">Semester 4</a></li>
+            <li><a href="#">Semester 5</a></li>
+            <li><a href="#">Semester 6</a></li>
+            <li><a href="#">Semester 7</a></li>
+            <li><a href="#">Semester 8</a></li>
+          </ul>
+         </li>
+            
+            <li class="dropdown">
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+              Form<span class="caret"></span>
+              </a>
+              
+              <ul class="dropdown-menu">
+                <li><a href="#">Action</a></li>
+                <li><a href="#">Another action</a></li>
+                <li><a href="#">Something else here</a></li>
+              </ul>
+            </li>
+          
+          </ul>
+        
+         <ul class="nav navbar-nav navbar-right">
+          <li class="dropdown-login">
+            
+            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false" >
+            Login<span class="caret"></span>
+            </a>
+            
+     	<ul class="dropdown-menu">
+<!-- login form----------------------------------------------------------------------------------->
+			<div id="login-content">
+        	
+            	<form class="form-signin">
+        
+        		<label for="inputEmail" class="sr-only">Email address</label>
+        		<input type="email" id="inputEmail" class="form-control" placeholder="Email address" required autofocus>
+        
+        		<label for="inputPassword" class="sr-only">Password</label>
+        		<input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+        
+        		<div class="checkbox">
+            	</div>
+        		<button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+        		</form>
+        	
+            </div>
+   		</ul>
+     </li>
+     </ul>
 
-<?php
-for($i=0;$i<count($tahunajaran);$i++){
-?>
-<option value="<?php echo $i+1;?>">
-<?php echo $tahunajaran[$i]; ?>
-</option>
-<?php
+</div>
+</div>
+</nav>
+<!-----------------end of navigation section--------------------------------------->
+
+    <!-- Page Content -->
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <p2>
+<form id="form1" name="form1" method="post" action="">
+  <table width="598" border="0">
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td> 
+    </tr>
+    <tr>
+      <td>Mata ajaran </td>
+      <td>:</td>
+      <td><input name="mataajar" type="text" value="Pengembangan SI" /></td>
+    </tr>
+    <tr>
+      <td>Kompetensi</td>
+      <td>:</td>
+      <td><textarea name="kompetensi"></textarea></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>Kompetensi Khusus </td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="subkomp" />
+      <input type="submit" name="Submit" value="tambah" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>Pokok Bahasan </td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="pokokbahasan" />
+      <input type="submit" name="Submit2" value="tambah" />
+      <input type="submit" name="Submit4" value="upload" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>Sub Pokok Bahasan </td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="text" name="subpokok" />
+      <input type="submit" name="Submit3" value="tambah" /></td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+    </tr>
+    <tr>
+      <td>&nbsp;</td>
+      <td>&nbsp;</td>
+      <td><input type="submit" name="kembali" value="back" />
+      <input type="submit" name="lanjut" value="next" /></td>
+    </tr>
+	  <?php
+if($_POST[lanjut]){
+	if ($_POST[mataajar] and $_POST[kompetensi] and $_POST [subkomp] and $_POST[pokokbahasan] and $_POST[subpokok]){
+	?>
+	<script>alert("Data Berhasil Terupdate")
+	window.location="kompetensi.php";
+	</script>
+	<?php
+	}
+	else
+	?>
+	<script>alert("form belum lengkap")
+	</script>
+	<?php
 }
 ?>
-           
-        
-      </select>
-    </form>    </td>
-  </tr>
-  <tr>
-    <td><label>Mata ajaran: </label>&nbsp;</td>
-    <td colspan="4"><input name="textfield2" type="text" value="Pengembangan SI" /></td>
-  </tr>
-  <tr>
-    <td><label>Kompetensi:</label>&nbsp;</td>
-    <td colspan="4" rowspan="2">&nbsp;</td>
-  </tr>
-  <tr>
-    <td><form id="form7" name="form7" method="post" action="">
-      <input type="text" name="textfield3" />
-    </form>    </td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td><label>Kompetensi khusus
-        <input name="textfield4" type="text" />
-        <input type="submit" name="Submit8" value="tambah" />
-    </label></td>
-    <td>&nbsp;</td>
-    <td colspan="2" rowspan="3">&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>Pokok bahasan
-    <input type="text" name="textfield5" />
-    <input type="submit" name="Submit6" value="tambah" /></td>
-    <td><input type="submit" name="Submit9" value="upload materi" /></td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td><label>Sub pokok bahasan
-        <input type="text" name="textfield6" />
-        <input type="submit" name="Submit7" value="tambah" />
-    </label></td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td>&nbsp;</td>
-    <td><input type="submit" name="Submit10" value="back" /></td>
-    <td><input type="submit" name="Submit11" value="next" /></td>
-  </tr>
-</table>
+  </table>
+
+</form>
+
+
+
+</p2>
+            </div>
+        </div>
+    </div>
+    <!-- /.container -->
+
+    <!-- jQuery -->
+    <script src="js/jquery.js"></script>
+
+    <!-- Bootstrap Core JavaScript -->
+    <script src="js/bootstrap.min.js"></script>
+
 </body>
+
 </html>
