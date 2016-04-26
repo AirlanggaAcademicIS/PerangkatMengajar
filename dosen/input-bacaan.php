edd1cf46a2c3246bb92b7b357f0fe4a1b9b38357
Shinta Mey Linawati <shintamey30@gmail.com>
2016-04-25 23:53:08 -0700

    untuk input bacaan baru
    menghapus kode mata ajar dari form yang lama

--- dosen/input-bacaan.php
+++ dosen/input-bacaan.php
@@ -1,62 +1,47 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>Untitled Document</title>
 </head>
 
 <body>
-<table width="500" border="1">
-  <tr>
-    <td>Kode Mata Ajar </td>
-  </tr>
-  <tr>
-    <td><label>
-    <input name="kodemataajar" type="text" id="kodemataajar" />
-    </label></td>
-  </tr>
-  <tr>
-    <td>Nama Pengarang </td>
-  </tr>
-  <tr>
-    <td><input name="namapengarang" type="text" id="namapengarang" /></td>
-  </tr>
-  <tr>
-    <td>Tanggal Terbit </td>
-  </tr>
-  <tr>
-    <td><label>
-      <input name="tanggalterbit" type="text" id="tanggalterbit" />
-    </label></td>
-  </tr>
-  <tr>
-    <td>Judul Buku </td>
-  </tr>
-  <tr>
-    <td><input name="judulbuku" type="text" id="judulbuku" /></td>
-  </tr>
-  <tr>
-    <td>Kota Tempat Terbit </td>
-  </tr>
-  <tr>
-    <td><input name="kotatempatterbit" type="text" id="kotatempatterbit" /></td>
-  </tr>
-  <tr>
-    <td>Nama Penerbit </td>
-  </tr>
-  <tr>
-    <td><input name="namapenerbit" type="text" id="namapenerbit" /></td>
-  </tr>
-  <tr>
-    <td>&nbsp;</td>
-  </tr>
-</table>
-<form id="form2" name="form2" method="post" action="">
-  <label></label>
-  <input type="submit" name="Save" value="Submit" />
-</form>
 <form id="form1" name="form1" method="post" action="">
-  <label></label>
+  <table width="297" border="1">
+    <tr>
+      <td width="128">Nama Pengarang</td>
+      <td width="7">:</td>
+      <td width="144"><label for="textfield"></label>
+      <input type="text" name="textfield" id="textfield" /></td>
+    </tr>
+    <tr>
+      <td>Tahun Terbit </td>
+      <td>:</td>
+      <td><label for="textfield2"></label>
+      <input type="text" name="textfield2" id="textfield2" /></td>
+    </tr>
+    <tr>
+      <td>Judul Buku </td>
+      <td>:</td>
+      <td><label for="textfield3"></label>
+      <input type="text" name="textfield3" id="textfield3" /></td>
+    </tr>
+    <tr>
+      <td>Kota Tempat Terbit </td>
+      <td>:</td>
+      <td><label for="textfield4"></label>
+      <input type="text" name="textfield4" id="textfield4" /></td>
+    </tr>
+    <tr>
+      <td>Nama Penerbit</td>
+      <td>:</td>
+      <td><label for="textfield5"></label>
+      <input type="text" name="textfield5" id="textfield5" /></td>
+    </tr>
+    <tr>
+      <td colspan="3" align="right"><input type="submit" name="button" id="button" value="SAVE" /></td>
+    </tr>
+  </table>
 </form>
 </body>
 </html>
