a1431381ae4e586fcfac71e1a41c7a245a0f59cb
arnandariz <arnanda.rizal@gmail.com>
2016-06-08 20:35:03 +0700

    navbar
    membetulkan dan merapikan navbar

--- Index.php
+++ Index.php
@@ -2,34 +2,33 @@
 <html lang="en">
 
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
     <meta name="author" content="">
-
     <title>e-PBM</title>
-
     <link href="Dosen/Assets/Css/bootstrap.min.css" rel="stylesheet">
     <link href="Dosen/Assets/Css/Layout.css" rel="stylesheet">
-
-
 </head>
 
 <body>
     <nav class="navbar navbar-inverse navbar-fixed-top">
         <div class="container">
             <div class="navbar-header">
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                </button>
                 <a class="navbar-brand"><img src="Dosen/Assets/Image/LogoEpbm.png" alt=""></a>
             </div>
-            	<div id="navbar">
-            		<ul class="nav navbar-nav">
-						<li class="active"><a href="#">Home</a></li>
+            <div id="navbar" class="navbar-collapse collapse">
+            	<ul class="nav navbar-nav">
+					<li><a href="#">Home</a></li>
 						<li class="dropdown">
-                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                            PBM<span class="caret"></span>
-                          </a>
+                          <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">PBM <span class="caret"></span></a>
           				<ul class="dropdown-menu">
                             <li><a href="#">Semester 1</a></li>
                             <li><a href="#">Semester 2</a></li>
@@ -52,32 +51,27 @@
                           </ul>
                         </li>
           			</ul>
-         <ul class="nav navbar-nav navbar-right">
-         <div id="navbar">
-            <ul class="nav navbar-nav">
-		        <li class="dropdown">
-        		<a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-            	Login<span class="caret"></span>
-          		</a>
-        			<ul class="dropdown-menu">
-						<form class="form-signin">
-        				<label for="inputNip">NIP</label>
-                        <input type="NIP" id="inputNIP" class="form-control" placeholder="NIP" required autofocus>
-                        <label for="inputPassword">Password</label>
-                        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
-        					<div class="checkbox"></div>
-                        <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
-                        </form>
-         			</ul>
-         		</li>
-     		</ul>
-		</div>
-	</div>
-</nav>
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Login<span class="caret"></span></a>
+                        <ul class="dropdown-menu">
+                            <form class="form-signin">
+                                <label for="inputNip">NIP</label>
+                                <input type="NIP" id="inputNIP" class="form-control" placeholder="NIP" required autofocus>
+                                <label for="inputPassword" style="padding-top: 5px;">Password</label>
+                                <input type="password" id="inputPassword" class="form-control" placeholder="Password" required>
+                                <div class="checkbox"></div>
+                                <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in</button>
+                            </form>
+                        </ul>
+                    </li>
+                </ul>
+            </div>
+       </div>
+    </nav>
 
     <script src="Dosen/Assets/Js/query.js"></script>
-
-    <!-- Bootstrap Core JavaScript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <script src="Dosen/Assets/Js/bootstrap.min.js"></script>
     <script src="Dosen/Assets/Js/Navbar.js"></script>
 </body>
